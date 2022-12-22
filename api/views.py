0f8d8b1dc5b0076617773268be38ca52db343044
Martin Yeghiazaryan <martineghiazaryan@gmail.com>
2022-12-22 23:49:46 +0400

    First Commit

--- api/views.py
+++ api/views.py
@@ -32,7 +32,7 @@ def getRoutes(request):
             'Endpoint': '/notes/id/delete/',
             'method': 'DELETE',
             'body': None,
-            'description': 'Deletes and exiting note'
+            # 'description': 'Deletes and exiting note'
         },
     ]
     return JsonResponse(routes, safe=False)
